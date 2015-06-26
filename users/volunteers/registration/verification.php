37631997da9e776246be363999cdbc792304c620
Mark <mrak.mk@ya.ru>
2015-06-26 21:02:24 +1000

    Work volon

--- users/volunteers/registration/verification.php
+++ users/volunteers/registration/verification.php
@@ -1,18 +1,17 @@
 ﻿<?php
-    include_once("bd.php");
-    function AddUser($LastName,$FirstName,$MiddleName,$DoB,$Gender,$Email,$MobilePhone,$Place,$Stud,$Social,$Size,$Growth,$Languages,$Password,$Password2,$DateTimeReg)
+    include_once $_SERVER['DOCUMENT_ROOT']."/cpvdvfu/system/bd.php";
+    function AddUser($LastName,$FirstName,$MiddleName,$DoB, $Gender,$Email,$MobilePhone,$Place,$Stud,$Social,$Size,$Growth,$Password,$Password2,$DateTimeReg)
     {
-    
         $err = array();
-        if (strlen($Password) < 3 or strlen($Password) > 30) {
+        /*if (strlen($Password) < 3 or strlen($Password) > 30) {
             $err[] = "Пароль должен быть не меньше трех и не больше тридцати символов!";
-        }
+        }*/
         # проверяем, не сущестует ли пользователя с таким именем
-        $query = ("SELECT VolunteerID FROM volunteers WHERE Email='$Email'");
-        			$sql = mysql_query($query);
-        			if (mysql_num_rows($sql) > 0) {
-        				$err[] = "Пользователь с таким email уже существует!";
-               			}
+        $query = ("SELECT VolunteerID FROM Volunteers WHERE Email='$Email'");
+                    $sql = mysql_query($query);
+                    if (mysql_num_rows($sql) > 0) {
+                        $err[] = "Пользователь с таким email уже существует!";
+                        }
         
         $mdPassword = md5($Password);
         $repeat   = md5($Password2);
@@ -20,27 +19,25 @@
             $err[] = "Пароли не совпадают! Попробуйте еще раз!";
         }
         # Если нет ошибок, то добавляем в БД нового пользователя
-        if (count($err) == 0) {
-
-           $query = "INSERT INTO Volunteers (LastName, FirstName, MiddleName, DoB, Gender, Email, MobilePhone, Place, Stud, Social, Size, Growth, Languages, Password, DateTimeReg)
-           		  VALUES ('$LastName', '$FirstName', '$MiddleName', '$DoB', '$Gender', '$Email', '$MobilePhone', '$Place', '$Stud', '$Social', '$Size', '$Growth', '$Languages', '$mdPassword', '$DateTimeReg')";
+        // if (count($err) == 0) {
+           $query = "INSERT INTO Volunteers (LastName, FirstName, MiddleName, DoB, Gender, Email, MobilePhone, Place, Stud, Social, Size, Growth, Password, DateTimeReg)
+                  VALUES ('$LastName', '$FirstName', '$MiddleName', $DoB, '$Gender', '$Email', '$MobilePhone', '$Place', '$Stud', '$Social', '$Size', '$Growth', '$mdPassword', '$DateTimeReg')";
            $result = mysql_query($query);
            return null;
-        }
+        // }
         return $err;
     }
     function action_newUser()
     { 
-
         if (isset($_POST['submitvolunteer'])) {
-            $error = AddUser($_POST['LastName'],$_POST['FirstName'],$_POST['MiddleName'],$_POST['DoB'],$_POST['Gender'],$_POST['Email'],$_POST['MobilePhone'],$_POST['Place'],$_POST['Stud'],$_POST['Social'],$_POST['Size'],$_POST['Growth'],$_POST['Languages'],$_POST['Password'],$_POST['Password2'],date("d-m-Y в H:i"));
+            $error = AddUser($_POST['LastName'],$_POST['FirstName'],$_POST['MiddleName'], $_POST['DoB'], $_POST['Gender'], $_POST['Email'],$_POST['MobilePhone'], $_POST['Place'],$_POST['Stud'],$_POST['Social'],$_POST['Size'],$_POST['Growth'],$_POST['Password'],$_POST['Password2'],date("d-m-Y в H:i"));
             if (count($error) == 0) {
                 echo '<font color="green">Вы успешно зарегистрировались!</font><br><a href="index.php">На главную</a>';
             } else {
                 foreach ($error as $value) 
-						{
-							echo('<br><div>'.$value.'</div>');
-						}	
+                        {
+                            echo('<br><div>'.$value.'</div>');
+                        }   
             }
         }
     }
